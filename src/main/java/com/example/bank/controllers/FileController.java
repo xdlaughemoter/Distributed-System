9d1a2559fc69424c90c00eb60c314cdba7419039
Max.etman <max.etman@student.kdg.be>
2026-04-01 12:45:15 +0200

    useless hash function removed

--- src/main/java/com/example/bank/controllers/FileController.java
+++ src/main/java/com/example/bank/controllers/FileController.java
@@ -39,24 +39,6 @@ public class FileController {
         this.fileClientService = fileClientService;
     }
 
-    private Integer hashingFunction(String input){
-        int hash = 0;
-        // P is a prime number (31 is common for ASCII strings)
-        int P = 31;
-        // M is a large prime to keep the hash within a specific range
-        int M = 1_000_000_009;
-
-        for (int i = 0; i < input.length(); i++) {
-            // Get the Unicode value of the character
-            char c = input.charAt(i);
-
-            // Formula: (hash * P + char_code) % M
-            hash = (hash * P + c) % M;
-        }
-
-        return hash;
-    }
-
     @PostMapping("/upload")
     public ResponseEntity<Resource> uploadFile(@RequestParam("file") MultipartFile file) {
         String filename = file.getOriginalFilename();
